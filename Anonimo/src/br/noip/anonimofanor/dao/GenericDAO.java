4a77fb3bed72cd409f1573936b5ad78e6f0e6e3a
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2014-05-16 01:05:44 -0300

    Dao genérico

---
+++ Anonimo/src/br/noip/anonimofanor/dao/GenericDAO.java
@@ -0,0 +1,17 @@
+package br.noip.anonimofanor.dao;
+
+import java.util.List;
+
+public interface GenericDAO<T, KEY> {
+	
+	public T salvar(T obj);
+	
+	public T atualizar(T obj);
+	
+	public T buscarPorId(KEY key);
+	
+	public List<T> listar();
+	
+	public void excluir(T obj);
+	
+}
---
+++ Anonimo/src/br/noip/anonimofanor/dao/GenericDAOImpl.java
@@ -0,0 +1,66 @@
+package br.noip.anonimofanor.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import biz.noip.anonimofanor.util.EntityManagerBuilder;
+
+public class GenericDAOImpl<T, KEY> implements GenericDAO<T, KEY>{
+
+	private EntityManager entityManager = EntityManagerBuilder.getEntityManager();
+	private Class<?> entityClass;
+	private String nameTable;
+	
+	public GenericDAOImpl(Class<?> entityClass) {
+		this.entityClass = entityClass;
+	}
+	
+	@Override
+	public T salvar(T obj) {
+		
+		this.entityManager.getTransaction().begin();
+		this.entityManager.persist(obj);
+		this.entityManager.getTransaction().commit();
+		
+		return obj;
+	}
+
+	@Override
+	public T atualizar(T obj) {
+		
+		this.entityManager.getTransaction().begin();
+		this.entityManager.merge(obj);
+		this.entityManager.getTransaction().commit();
+		
+		return obj;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T buscarPorId(KEY key) {
+		return (T) this.entityManager.find(this.entityClass, key);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> listar() {
+		Query query = this.entityManager.createQuery("SELECT t FROM " + this.nameTable, this.entityClass);
+		List<T> list = (List<T>) query.getResultList();
+		this.entityManager.close();
+		
+		return list;
+	}
+
+	@Override
+	public void excluir(T obj) {
+		this.entityManager.getTransaction().begin();
+		this.entityManager.remove(obj);
+		this.entityManager.getTransaction().commit();
+		this.entityManager.close();
+	}
+	
+	
+	
+}
