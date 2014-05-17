cbcf7d299d3d8ce03a3016a6d65f7720e8216740
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2014-05-16 22:09:34 -0300

    Entidade Perfil

---
+++ Anonimo/src/biz/noip/anonimofanor/entity/Perfil.java
@@ -0,0 +1,78 @@
+package biz.noip.anonimofanor.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Perfil implements Serializable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1581723122386319140L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_perfil")
+	private Long id;
+	
+	@Column(length=120, nullable=false)
+	private String nome;
+	
+	@Column(nullable=true)
+	private Integer idade;
+	
+	@Column(length=45, nullable=true)
+	private String escolaridade;
+	
+	@OneToMany
+	private List<Resumo> resumos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public String getEscolaridade() {
+		return escolaridade;
+	}
+
+	public void setEscolaridade(String escolaridade) {
+		this.escolaridade = escolaridade;
+	}
+
+	public List<Resumo> getResumos() {
+		return resumos;
+	}
+
+	public void setResumos(List<Resumo> resumos) {
+		this.resumos = resumos;
+	}
+}
