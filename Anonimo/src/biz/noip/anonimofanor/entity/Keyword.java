5fd7d8f8200fa56149b7d4b971402c646d141c2d
Administrador <Administrador@MiguelJefferson>
2014-05-21 00:21:31 -0300

    Entity Keyword

--- Anonimo/src/biz/noip/anonimofanor/entity/Keyword.java
+++ Anonimo/src/biz/noip/anonimofanor/entity/Keyword.java
@@ -2,7 +2,6 @@ package biz.noip.anonimofanor.entity;
 
 import java.io.Serializable;
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,20 +12,16 @@ import javax.persistence.ManyToMany;
 @Entity
 public class Keyword implements Serializable {
 
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1657923805450134393L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id_keyword")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id_keyword")
 	private Long id;
-	
-	@Column(nullable=false, unique=true, length=45)
+
+	@Column(nullable = false, unique = true, length = 45)
 	private String slug;
-	
+
 	@ManyToMany
 	private List<Resumo> resumos;
 
@@ -53,6 +48,5 @@ public class Keyword implements Serializable {
 	public void setResumos(List<Resumo> resumos) {
 		this.resumos = resumos;
 	}
-	
-	
+
 }
